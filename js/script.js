573564e80a35acd84485fe4677823c92853e2e50
William Yeung <williamyeung1141998@gmail.com>
2019-05-22 17:20:49 -0400

    Created array of quote objects

--- js/script.js
+++ js/script.js
@@ -14,7 +14,41 @@ project 1 - A Random Quote Generator
   Add the `year` property to at least one object in the array.
   Use console.log() to log your array of quotes to the console.
 ***/
-
+let quotes = [{quote: "I say to you today, my friends, that in spite of the difficulties and frustrations of the moment, I still have a dream.",
+               source: "Martin Luther King, Jr.",
+               citation: "Speech",
+               year: 1963
+              }, 
+            
+              {quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+                source: "Nelson Mandela",
+                citation: undefined,
+                year: undefined
+               },
+              
+               {quote: "I hated every minute of training, but I said, ‘Don’t quit. Suffer now and live the rest of your life as a champion.",
+                source: "Muhammad Ali",
+                citation: undefined,
+                year: undefined
+               },
+              
+               {quote: "Do. Or do not. There is no try.",
+                source: "Yoda",
+                citation: "The Empire Strikes Back",
+                year: 1980
+               },
+               
+               {quote: "With great power, comes great responsibility.",
+                source: "Uncle Ben",
+                citation: "Spider-Man",
+                year: 2002
+               },
+              
+               {quote: "You will face many defeats in life, but never let yourself be defeated.",
+                source: "Maya Angelou",
+                citation: undefined,
+                year: undefined
+               },]
 
 
 
