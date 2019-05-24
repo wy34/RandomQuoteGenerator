0a95cb91a338bf4e066a991f6c3f9d7d63a54f3d
William Yeung <williamyeung1141998@gmail.com>
2019-05-23 21:25:18 -0400

    Project 1 Final Version

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ William Yeung
 
 
 
-// An array of quote objects to store not only quotes, but mutliple of other components related to the quotes
+// An array of quote objects to store not only individual quotes, but mutliple of other components related to the quotes
 let quotes = [{quote: "I say to you today, my friends, that in spite of the difficulties and frustrations of the moment, I still have a dream.",
                source: "Martin Luther King, Jr.",
                citation: "'I Have a Dream' speech",
@@ -76,10 +76,10 @@ function getRandomBkgrndColor() {
 
   let blue = Math.floor(Math.random() * 256);
 
-  let rgbColor = `rgb(${red}, ${green}, ${blue})`
+  let rgbColor = `rgb(${red}, ${green}, ${blue})`;
 
   // "connects" the generated JS to the css???
-  document.body.style.background = rgbColor
+  document.body.style.background = rgbColor;
 }
 
 
@@ -111,21 +111,21 @@ function printQuote() {
 
   // have to check for these two properties because not all quotes have them
   if (retrievedQuote.citation) {
-    HTML += `<span class="citation">${retrievedQuote.citation}</span>`
+    HTML += `<span class="citation">${retrievedQuote.citation}</span>`;
   }
 
   if (retrievedQuote.year) {
-    HTML += `<span class="year">${retrievedQuote.year}</span>`
+    HTML += `<span class="year">${retrievedQuote.year}</span>`;
   }
 
-  HTML += `<p class="tags"><em>#${retrievedQuote.tags[0]} #${retrievedQuote.tags[1]} #${retrievedQuote.tags[2]}</em></p>`
+  HTML += `<p class="tags"><em>#${retrievedQuote.tags[0]} #${retrievedQuote.tags[1]} #${retrievedQuote.tags[2]}</em></p>`;
 
-  HTML += `</p>`
+  HTML += `</p>`;
 
   /* enables the html created in javascript code to essentially switch with the current stuff inside the 
   div: quote-box in index.html file with our own quotes and be able to acually show up and change in the designated 
   area of website when clicked by a button */
-  document.getElementById('quote-box').innerHTML = HTML 
+  document.getElementById('quote-box').innerHTML = HTML;
 }
 
 
@@ -136,7 +136,7 @@ function refresh(interval) {
   setInterval(printQuote, interval);
   setInterval(getRandomBkgrndColor, interval);
 }
-refresh(20000) // page changes every 20 seconds
+refresh(20000); // page changes every 20 seconds
 
 
 
